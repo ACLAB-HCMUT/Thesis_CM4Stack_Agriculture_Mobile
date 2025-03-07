6d683c9bda8d82d9c887531725c1bdf0b1b5da7d
Nguyen Quoc Huy <95513347+NQHuy14@users.noreply.github.com>
2025-03-07 16:46:29 +0700

    init project
    init project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,11 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -8,7 +14,7 @@ plugins {
 android {
     namespace = "com.greencorp.greenfarm.triple_h"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,17 +30,56 @@ android {
         applicationId = "com.greencorp.greenfarm.triple_h"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 23
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+    flavorDimensions += "environment"
+
+    productFlavors {
+        create("dev") {
+            dimension = "environment"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+            manifestPlaceholders["flutterTarget"] = "lib/main_dev.dart"
+        }
+        create("staging") {
+            dimension = "environment"
+            // Final Application ID: com.yourcompany.yourapp.staging
+            applicationIdSuffix = ".staging"
+            versionNameSuffix = "-staging"
+        }
+        create("prod") {
+            dimension = "environment"
+            // Final Application ID: com.yourcompany.yourapp
+        }
+    }
+
+    // Load keystore properties from key.properties file
+    val keystoreProperties = Properties()
+    //val keystorePropertiesFile = file("C:\\Users\\Huy\\Desktop\\remote_control\\ci_cd_testing\\ci_cd_testing\\key.properties")
+    val keystorePropertiesFile = file("key.properties")
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            // Configure proguard if necessary
+        }
+        debug {
+            // Debug builds are not signed for release, usually.
         }
     }
 }
