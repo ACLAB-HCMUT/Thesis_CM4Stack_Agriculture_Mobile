1093008583bbf0dbce95f545a23ff8bb8afa58e9
Nguyen Quoc Huy <95513347+NQHuy14@users.noreply.github.com>
2025-03-13 00:21:33 +0700

    change version
    change version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 23
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.1"
     }
     flavorDimensions += "environment"
 
