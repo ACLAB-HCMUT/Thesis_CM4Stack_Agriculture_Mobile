787c844466760cf95b0b3655f2f63795ef763f2d
NQHuy14 <95513347+NQHuy14@users.noreply.github.com>
2025-03-13 11:12:41 +0700

    Revert "Init project"

--- lib/main_dev.dart
+++
@@ -1,9 +0,0 @@
-import 'main_common.dart';
-import 'firebase_options_dev.dart';
-
-Future<void> main() async {
-  await mainCommon(
-    flavor: 'dev',
-    firebaseOptions: DefaultFirebaseOptions.currentPlatform,
-  );
-}
