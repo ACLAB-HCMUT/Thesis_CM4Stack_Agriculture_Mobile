c211ad2e966fd901ef7a67863b517c8a66243d4d
HVM Hieu <huavuminhhieu.cx262002@gmail.com>
2025-03-17 14:09:24 +0700

    undo build gradle
    undo build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,8 @@ plugins {
 }
 
 android {
-    namespace = "com.greencorp.greenfarm.triple_h.dev"
+//    namespace = "com.greencorp.greenfarm.triple_h.dev"
+    namespace = "com.greencorp.greenfarm.triple_h"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -27,7 +28,8 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.greencorp.greenfarm.triple_h.dev"
+//        applicationId = "com.greencorp.greenfarm.triple_h.dev"
+        applicationId = "com.greencorp.greenfarm.triple_h"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
@@ -40,25 +42,26 @@ android {
     productFlavors {
         create("dev") {
             dimension = "environment"
-//            applicationIdSuffix = ".dev"
+            applicationIdSuffix = ".dev"
             versionNameSuffix = "-dev"
             manifestPlaceholders["flutterTarget"] = "lib/main_dev.dart"
         }
-//        create("staging") {
-//            dimension = "environment"
-//            // Final Application ID: com.yourcompany.yourapp.staging
-//            applicationIdSuffix = ".staging"
-//            versionNameSuffix = "-staging"
-//        }
-//        create("prod") {
-//            dimension = "environment"
-//            // Final Application ID: com.yourcompany.yourapp
-//        }
+        create("staging") {
+            dimension = "environment"
+            // Final Application ID: com.yourcompany.yourapp.staging
+            applicationIdSuffix = ".staging"
+            versionNameSuffix = "-staging"
+        }
+        create("prod") {
+            dimension = "environment"
+            // Final Application ID: com.yourcompany.yourapp
+        }
     }
 
 //     Load keystore properties from key.properties file
     val keystoreProperties = Properties()
-    val keystorePropertiesFile = rootProject.file("key.properties")
+    val keystorePropertiesFile = file("key.properties")
+//    val keystorePropertiesFile = rootProject.file("key.properties")
     if (keystorePropertiesFile.exists()) {
         keystoreProperties.load(FileInputStream(keystorePropertiesFile))
     }
@@ -67,7 +70,8 @@ android {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storeFile = file(keystoreProperties["storeFile"] as String)
+//            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
             storePassword = keystoreProperties["storePassword"] as String
         }
     }
@@ -78,11 +82,16 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now,
             // so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+//            signingConfig = signingConfigs.getByName("debug")
             signingConfig = signingConfigs.getByName("release")
         }
+        debug {
+            // TODO: Add your own signing config for the debug build.
+            // Signing with the debug keys for now,
+            // so `flutter run` works.
+//            signingConfig = signingConfigs.getByName("debug")
     }
-    }
+}
 
     flutter {
         source = "../.."
